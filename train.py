748da88f5b6b7b9ce636f5709cdefaf8fa2241ea
terencemui <mui.terence1@gmail.com>
2025-02-24 14:50:02 -0800

    mem summary

--- train.py
+++ train.py
@@ -232,6 +232,7 @@ def train_model(model, train_loader, val_loader, epochs, writer, verbose=True):
         model.save_pretrained(f"weights/weights_{run_number}/epoch_{epoch}")
         torch.save(optimizer.state_dict(), f"weights/weights_{run_number}/epoch_{epoch}/optimizer.pt")
 
+        torch.cuda.memory_summary()
         # clear_gpu_cache()
 
     return
